4198977ac0e56f91850063f6ee46a78ff5ae130f
Steven Vercruysse <stcruy@users.noreply.github.com>
2020-05-18 23:57:10 +0200

    update sanitizeHtml test

--- src/subcomponents/sanitizeHtml.test.js
+++ src/subcomponents/sanitizeHtml.test.js
@@ -3,7 +3,7 @@ import sanitizeHtml from './sanitizeHtml.js';
 
 describe('sub/sanitizeHtml()', () => {
 
-  it.only('HTML-encodes the opening tag of unsafe HTML-tags, and not of other tags', () => {
+  it('HTML-encodes the opening tag of unsafe HTML-tags, and not of other tags', () => {
     sanitizeHtml('<x7>"<script>< script="a"><iframe>< Style><textARea\n>')
       .should.equal(
         '<x7>"&lt;script>&lt; script="a">&lt;iframe>&lt; Style>&lt;textARea\n>'
